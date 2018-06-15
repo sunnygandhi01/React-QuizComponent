445810f16ec8314e4b3055e2a3cdb51cc5148870
Sunny Gandhi <sunnygandhi01@gmail.com>
2018-06-15 09:30:46 -0500

    module_4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
   constructor(props) {
     super(props)
-    this.state = { quiz_position: 1 }
+    this.state = { quiz_position: 1 };
   }
   render() {
+    const isQuizEnd;
+    
+    if ((this.state.quiz_position - 1) === quizData.quiz_questions.length) {
+      isQuizEnd = true;
+    } else {
+      isQuizEnd = false;
+    }
     return (
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      if(isQuizEnd = false) {
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      } else {
+        <QuizEnd />
+      }
     );
   }
 }
